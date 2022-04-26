f4691c25516c5d814b3cf2c0489931ccd10dffbe
Mustafa <mkafridi073@gmail.com>
2022-04-26 13:07:06 +0500

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -33,6 +33,36 @@ router.post('/signup', (req,res,next) =>{
 });
 });
 router.post('/login', (req,res,next) =>  {  
-  
+  if(!req.session.user) {
+    var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  user.findOne({username: user})
+  .then((user) => {
+    if( user === null) {
+      var err = new Error('User' + user + ' does not exist!');
+      err.statusCode = 403;
+      next(err);
+    }
+    if (user === 'admin' && pass === 'password') {
+      req.session.user = 'admin';
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+  }) 
+}
 });
 module.exports = router;

d320b7a06d4d38e51df5332bc8df0f7afd741894
Mustafa <mkafridi073@gmail.com>
2022-04-29 09:46:47 +0500

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -12,16 +12,15 @@ router.use(bodyParser.json());
   res.send('Hello there! Please respond with a resource');
 });
 router.post('/signup', (req, res, next) => {
-  User.findOne({username: req.body.username})
-  .then((user) => {
-    if(user != null) {
-      var err = new Error('User ' + req.body.username + ' already exists!');
-      err.status = 403;
-      next(err);
+  User.register(new User({username: req.body.username}), 
+  req.body.password, (err, user) => {
+    if(err) {
+      return res.status(500).json({err: err});
     }
     else {
-      return User.create
-      ({username: req.body.username,password: req.body.password});
+      passport.authenticate('local')(req, res, () => {
+        return res.status(200).json({status: 'Registration Successful!'});
+      });
     }
   })
   .then((user) => {

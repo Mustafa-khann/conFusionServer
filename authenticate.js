63c9814e57c587a67a2a15e74e09f7dcc98e9318
Mustafa <mkafridi073@gmail.com>
2022-04-30 00:39:41 +0500

    minor changes

--- authenticate.js
+++ authenticate.js
@@ -2,6 +2,8 @@ var passport = require(passport);
 var localStrategy = require(passport-local);
 var User = require('./models/user');
 
+
 exports.local = passport.use(new localStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());

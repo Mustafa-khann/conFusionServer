102ef89dc309c8fa391d9b58e145fdbf38437695
Mustafa <mkafridi073@gmail.com>
2022-04-30 10:01:58 +0500

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -38,7 +38,7 @@ router.post('/signup', (req, res, next) => {
 router.post('/login', passport.authenticate('local'), (req,res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({status: 'Login Successful!', user: req.user});
+  res.json({success: true, status: 'Login Successful!', user: req.user});
 })
 
 router.get('/logout', (req, res) => {

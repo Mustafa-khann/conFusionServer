2ab370f0af195e37d76ab2d713316f1eab1ef7ae
Mustafa <mkafridi073@gmail.com>
2022-05-12 12:51:56 +0500

    minor changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 //Routes
 var indexRouter = require('./routes/index');
@@ -22,7 +23,7 @@ const Dishes = require('./models/dishes');
 const { appendFile } = require('fs');
 
 //Database Connection
-const url = 'mongodb://localhost:27017/conFusion';
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 //Assurance of Connection to the DB
@@ -38,31 +39,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321-23423',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-function auth (req, res, next) {
- 
-  if(!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  }
-  else {
-      next();
-  }
-}
-app.use(auth);
 
 // endpoints
 app.use('/dishes', dishRouter)

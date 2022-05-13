bf43c2e07cd7729a97bf3f4b37540776a6398af4
Mustafa <mkafridi073@gmail.com>
2022-05-13 10:04:55 +0500

    promousers are using jwt authentication

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const Promotions = require('../models/promotions');
 const promoRouter = express.Router();
-
+const authenticate = require('../authenticate');
 promoRouter.use(bodyParser.json());
 
 promoRouter.route('/')
@@ -16,7 +16,7 @@ promoRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     Promotions.create(req.params.body)
     .then((promotion) => {
         res.statusCode = 200;
@@ -25,11 +25,11 @@ promoRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Promotions.remove({})
     .then((resp) => {
         res.statusCode = 200;
@@ -49,11 +49,11 @@ promoRouter.route('/:promoId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, (req, res, next) => {
     res.statusCode = 403;
     res.end('POST operation not supported on /promotions/'+ req.params.promoId);
 })
-.put((req, res, next) => {
+.put(authenticate.verifyUser, (req, res, next) => {
     Promotions.findByIdAndUpdate(req.params.promoId, 
         {$set: req.body}, {new: true})
         .then((promotion) => {
@@ -63,7 +63,7 @@ promoRouter.route('/:promoId')
         }, (err) => next(err))
         .catch((err) => next(err)); 
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, (req, res, next) => {
     Promotions.findOneAndRemove(req.params.promoId)
     .then((respooooonse) => {
         res.statusCode = 200;

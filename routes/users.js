83b6be41731317eadff1496f3d99a8acc26f0c06
Mustafa <mkafridi073@gmail.com>
2022-06-25 19:30:49 +0500

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ router.use(bodyParser.json());
 
 
 /* GET users listing. */
-  router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('Hello there! Please respond with a resource');
 });
 router.post('/signup', (req, res, next) => {
@@ -22,22 +22,23 @@ router.post('/signup', (req, res, next) => {
     }
     else {
       if (req.body.firstname)
-        user.firstname = req.body.firstname;
+      user.firstname = req.body.firstname;
       if (req.body.lastname)
-        user.lastname = req.body.lastname;
-      user.save((err, user) => {
-        if (err) {
+      user.lastname = req.body.lastname;
+      user.save((err,user) => {
+        if(err)
+        {
           res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
+          res.setHeader("Content-Type", "application/json");
           res.json({err: err});
-          return ;
+          return
         }
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({success: true, status: 'Registration Successful!'});
-        });
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, status: 'Registration Successful!'});
       });
+    });
     }
   });
 });

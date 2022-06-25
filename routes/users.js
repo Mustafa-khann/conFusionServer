16caf30cdafe3f4e4b1820e27f65f58e5c0baccf
Mustafa <mkafridi073@gmail.com>
2022-06-25 14:37:47 +0500

    minor changes

--- routes/users.js
+++ routes/users.js
@@ -12,31 +12,33 @@ router.use(bodyParser.json());
   res.send('Hello there! Please respond with a resource');
 });
 router.post('/signup', (req, res, next) => {
-  User.register(new User({username: req.body.username}), req.body.password, (err, user) => {      
-      if(err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({err: err});
-      }
-      else {
-        if(req.body.firstname)
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      if (req.body.firstname)
         user.firstname = req.body.firstname;
-        if(req.body.lastname)
+      if (req.body.lastname)
         user.lastname = req.body.lastname;
-        user.save((err,user) => {
-          if(err) {
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({err: err});
-          }
-            passport.authenticate('local')(req, res, () => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({success: true, status: 'Registration Successful!'});
-          });
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err});
+          return ;
+        }
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful!'});
         });
-      }
-    });
+      });
+    }
+  });
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
